7f3830248352cf9745ac6253e716a8ef9b3ea9be
Runar Ingebrigtsen <runar@rin.no>
2012-04-16 17:21:40 +0200

    actually include the browserid helper script...

--- lib/devise-browserid/view_helpers.rb
+++ lib/devise-browserid/view_helpers.rb
@@ -1,7 +1,7 @@
 module BrowserId
   module ViewHelpers
     def browserid_include_tag
-      raw %{<script src="https://#{ request.env['warden'].config.browserid_url }/include.js" type="text/javascript"></script>\n#{ javascript_include_tag "application" }}
+      raw %{<script src="https://#{ request.env['warden'].config.browserid_url }/include.js" type="text/javascript"></script>\n#{ javascript_include_tag "browserid" }}
     end
 
     def browserid_login_tag
