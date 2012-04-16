2de0ec3b726b249d893f22eba052a20f6daa086f
Runar Ingebrigtsen <runar@rin.no>
2012-04-16 17:25:39 +0200

    Head's not working. Use Rails default methods.

--- lib/devise-browserid/view_helpers.rb
+++ lib/devise-browserid/view_helpers.rb
@@ -1,7 +1,7 @@
 module BrowserId
   module ViewHelpers
     def browserid_include_tag
-      raw %{<script src="https://#{ request.env['warden'].config.browserid_url }/include.js" type="text/javascript"></script>\n#{ javascript_include_tag "browserid" }}
+      javascript_include_tag("https://#{ request.env['warden'].config.browserid_url }/include.js", "browserid")
     end
 
     def browserid_login_tag
